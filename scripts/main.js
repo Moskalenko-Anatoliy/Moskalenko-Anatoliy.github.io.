285638d468084b4e8b7cef6f54b16cf44723146f
Moskalenko-Anatoliy <a.moskalenko@rtit.ru>
2020-08-21 18:40:44 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/cosmonaut.png') {
+      myImage.setAttribute ('src','images/space.jpg');
+    } else {
+      myImage.setAttribute ('src','images/cosmonaut.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
